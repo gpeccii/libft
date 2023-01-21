a6e483d6ae6de059d2307193f043cf2333849f72
gp <gp@mbpro.local>
2023-01-21 17:44:57 +0100

    end 21.1

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gpecci <gpecci@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 14:56:38 by gpecci            #+#    #+#             */
-/*   Updated: 2023/01/21 15:56:17 by gpecci           ###   ########.fr       */
+/*   Updated: 2023/01/21 17:44:15 by gpecci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*str;
 
-	if (!s)
+	if (s == NULL || start >= ft_strlen(s))
 		return (NULL);
-	if (start > ft_strlen(s))
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
+	str = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+			str[j++] = s[i];
+		i++;
+	}
+	str[j] = '\0';
+	return (str);
 }
